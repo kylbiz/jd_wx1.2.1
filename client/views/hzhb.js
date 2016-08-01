edf825dbf30b3f2e74ba38c9e6d20ac6bc8144a5
arronf2e <191446367@qq.com>
2016-08-01 11:17:22 +0800

    Update

--- client/views/hzhb.js
+++ client/views/hzhb.js
@@ -1,90 +1,74 @@
 Template.hzhb.helpers({
-	shops: function () {
-		return [
-			{
-				img: '../image/project/嘉境通logo.png',
-				name: '嘉境通跨境电商平台',
-				enname: 'Jiading Gateway',
-				intro: '上海嘉境通国际贸易有限公司于2015年1月8日由“上海跨境通”与“嘉定区出口加工区”合资成立。嘉境通在嘉定出口加工区内搭建了跨境贸易电子商务的专业仓储设施，为在嘉定区跨境电商企业从事跨境贸易提供高效、便利的仓储和报关报检等服务。',
-				url: '/ptjs/1'
-			},
-			{
-				img: '../image/project/hrlogo.png',
-				name: '汇鼎（上海）汽车销售有限公司',
-				enname: 'HuiDing GROUP',
-				intro: '汇瑞集团由汇力基金投资，注册资本500,000,000元。董事长孟晓苏先生曾任国家进出口检验局副局长、中房集团董事长等职务，现任中房集团理事长、幸福人寿监事长、汇力基金董事长、中国企业家联合会执行副会长。',
-				url: '/ptjs/2'
-			},
-			{
-				img: '../image/hzhb/cpl.png',
-				name: '上海马珂博逻跨境电商综合运营平台',
-				enname: 'CPL',
-				intro: '上海马珂博逻电子商务有限公司致力于为国内外商品厂商，电子商务销售平台提供集合建站、货源分销、商品运营，管理软件开发应用、跨国运输、国内外仓储订单履约等一站式服务。',
-				url: ''
-			}
-		]
+	shops: function() {
+		return [{
+			img: '../image/project/嘉境通logo.png',
+			name: '嘉境通跨境电商平台',
+			enname: 'Jiading Gateway',
+			intro: '上海嘉境通国际贸易有限公司于2015年1月8日由“上海跨境通”与“嘉定区出口加工区”合资成立。嘉境通在嘉定出口加工区内搭建了跨境贸易电子商务的专业仓储设施，为在嘉定区跨境电商企业从事跨境贸易提供高效、便利的仓储和报关报检等服务。',
+			url: '/ptjs/1'
+		}, {
+			img: '../image/project/hrlogo.png',
+			name: '汇鼎（上海）汽车销售有限公司',
+			enname: 'HuiDing GROUP',
+			intro: '是由汇瑞集团投资的上海汇车供应链管理有限公司具体运行，建立平行车进口及售后服务一体化平台，建立汽车及零部件展示交易跨境O2O服务平台，建立高档品牌进口车零配件区域保税集散服务中心（亚太或中国）将打造成立足上海、辐射华东，面向全国及亚太地区的汽车及零部件的展示交易中心，成为国内汽车行业的桥头堡。项目重点打造三大中心：1.汽车展示体验交易中心 2.汽车结算金融服务中心 3.汽车及零部件研发中心。',
+			url: '/ptjs/2'
+		}, {
+			img: '../image/hzhb/cpl.png',
+			name: '上海马珂博逻跨境电商综合运营平台',
+			enname: 'MCPL',
+			intro: '马珂博逻跨境电商综合运营服务平台是完全按照海关、国检相关政策法规要求打造的一站式跨境电商服务平台，是上海首批与市跨境电商公共服务平台签署战略合作协议的电商企业。通过与市公共平台数据对接，马珂博逻跨境电商服务平台已实现将其设在全球二十八个国家与地区主要跨境商品源的海外仓业务整合，并已经开通了上海浦东、虹桥国际机场的跨境直邮业务，平台通过与国内各大电商的数据对接，为从事跨境电商业务的网站、网店、微商等提供综合跨境全供应链业务服务。',
+			url: ''
+		}]
 	}
 });
 
 Template.zqqy.helpers({
-	companys: function () {
-		return [
-			{
+	companys: function() {
+		return [{
 				img: '../image/hzhb/zqqy1.png',
 				name: '乔山健身器材（上海）有限公司',
 				url: ''
-			},
-			{
+			}, {
 				img: '../image/hzhb/zqqy2.png',
 				name: '欧坚云贸通',
 				url: ''
-			},
-			{
+			}, {
 				img: '../image/hzhb/zqqy3.png',
 				name: '上海嘉定对外贸易有限公司',
 				url: ''
-			},
-			{
+			}, {
 				img: '../image/hzhb/zqqy4.png',
 				name: '上海普力姆物流有限公司',
 				url: ''
-			},
-			{
+			}, {
 				img: '../image/hzhb/zqqy5.png',
 				name: '上海兴亚报关报关有限公司',
 				url: ''
-			},
-			{
+			}, {
 				img: '../image/hzhb/zqqy6.png',
 				name: '上海丝嘉货运代理有限公司',
 				url: ''
-			},
-			{
+			}, {
 				img: '../image/hzhb/zqqy7.png',
 				name: '上海泰士星日用制品有限公司',
 				url: ''
-			},
-			{
+			}, {
 				img: '../image/hzhb/zqqy8.png',
 				name: '上海唯可以行物流有限公司',
 				url: ''
-			},
-			{
+			}, {
 				img: '../image/project/嘉境通logo.png',
 				name: '嘉境通',
 				url: ''
-			},
-			{
+			}, {
 				img: '../image/project/hrlogo.png',
 				name: '上海汇车供应链管理有限公司',
 				url: ''
-			},
-			{
+			}, {
 				img: '../image/project/pro-icon.png',
 				name: '智贸通',
 				url: ''
-			},
-			{
+			}, {
 				img: '../image/project/jdlogo.png',
 				name: '京东“跨境一号”',
 				url: ''
